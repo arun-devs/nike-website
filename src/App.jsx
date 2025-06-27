aa86e5ded228578c43f2955aad2582b425e79b62
Arun <arundevarajan346@gmail.com>
2025-06-27 23:31:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-500 border-spacing-1'>hello arun</div>
+    
+    </>
+  )
+}
+
+export default App
